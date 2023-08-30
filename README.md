c8587cbdc55d995ba107ebe294d0b6be0523fa93
Giordano <121612271+Gio1804@users.noreply.github.com>
2023-08-30 21:27:01 +0200

    aggiunto README

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# S2I-counter
+# S2I-Counter
+
+![Counter App Screenshot](img/screenshot.png)
+
+## Introduzione
+Benvenuti nel mio progetto Start2Impact/JavaScript Basics. Questo counter, realizzato in JavaScript, permette di aumentare, diminuire e reimpostare un valore numerico.
+
+## Installazione
+1. Clona questa repository: `https://github.com/Gio1804/S2I-counter.git`
+2. Apri il file index.html nel tuo browser.
+
+## Uso
+- Fai clic sul pulsante "+" per aumentare il contatore.
+- Fai clic sul pulsante "-" per diminuire il contatore.
+- Fai clic sul pulsante "Reset" per reimpostare il contatore a zero.
+
+## Anteprima
+![Counter App Preview](img/preview.gif)
+
+## Link al Progetto
+Puoi trovare il progetto completo sul mio [GitHub Repository](https://github.com/Gio1804/S2I-counter)
+
+## Licenza
+Questo progetto è distribuito con la licenza MIT. 
+
+## Contatti
+Se hai domande, feedback o vuoi collaborare, puoi contattarmi all'indirizzo: giordanorimato@gmail.com
--- js/app.js
+++ js/app.js
@@ -1,26 +1,68 @@
 let counter = 0;
 
-const counterValue = document.getElementById('counter-value');
-const incrementBtn = document.getElementById('increment-btn');
-const decrementBtn = document.getElementById('decrement-btn');
-const resetBtn = document.querySelector('#reset')
+// div con classe "container"
+const containerDiv = document.createElement("div");
+containerDiv.className = "container";
 
-//Per incrementare il valore del counter
-incrementBtn.addEventListener('click', () => {
+// div con classe "counter"
+const counterDiv = document.createElement("div");
+counterDiv.className = "counter";
+
+// Creo h1
+const titleElement = document.createElement("h1");
+titleElement.textContent = "Counter";
+
+// Creo bottone incremento con id
+const incrementButton = document.createElement("button");
+incrementButton.id = "increment-btn";
+incrementButton.textContent = "+";
+// Incremento valore counter
+incrementButton.addEventListener('click', () => {
     counter++;
-    counterValue.innerHTML = counter;
-});
-
-//Per diminuire il valore del counter
-decrementBtn.addEventListener('click', () => {
-    counter--;
-    counterValue.innerHTML = counter;
+    counterValueDiv.innerHTML = counter;
 })
 
-//Per azzerare il counter
-resetBtn.addEventListener('click', reset)
+// Creo il div e id per valore predefinito 0
+const counterValueDiv = document.createElement("button");
+counterValueDiv.id = "counter-value";
+counterValueDiv.textContent= "0";
+
+// Creo bottone decremento e id
+const decrementButton = document.createElement("button");
+decrementButton.id = "decrement-btn";
+decrementButton.textContent = "-";
+// Decremento valore counter
+decrementButton.addEventListener('click', () => {
+    if(counter > 0){ // Evita che il counter vado sotto lo zero
+    counter--;
+    counterValueDiv.innerHTML = counter;
+    }
+})
+
+// Creo bottone reset e id
+const resetButton = document.createElement("button");
+resetButton.id = "reset";
+resetButton.textContent = "Reset";
+// Reset counter
+resetButton.addEventListener('click', reset)
 
 function reset(){
-    counter = 0;
-    counterValue.innerHTML = counter;
+    counter = 0 ;
+    counterValueDiv.innerHTML = counter
 }
+
+// Aggiungo elementi al DOM
+counterDiv.appendChild(titleElement);
+counterDiv.appendChild(incrementButton);
+counterDiv.appendChild(counterValueDiv);
+counterDiv.appendChild(decrementButton);
+counterDiv.appendChild(resetButton);
+
+containerDiv.appendChild(counterDiv);
+
+// Container nell'elemento body
+document.body.appendChild(containerDiv);
+
+
+
+
